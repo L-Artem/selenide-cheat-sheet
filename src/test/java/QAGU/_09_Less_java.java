ca31e0b016c6200d74e24bde2319bbe217c94d8a
L-Artem <plazalavz@gmail.com>
2024-02-01 09:03:16 +0300

    001 - 09les

---
+++ src/test/java/QAGU/_09_Less_java.java
@@ -0,0 +1,16 @@
+package QAGU;
+
+public class _09_Less_java {
+
+    //наименование пакетов
+
+    //Типы данных (примитивные)
+    //Логический
+    boolean varBoolean = true;
+    //Целочисленный
+    byte varByte; //8 бит  -128...127  (-2^7...2^7-1) два в седьмой степени минус один
+    short varShort; //16 бит -321768...321767
+    int varInt; //32 бит
+    long varLong; //64 бит
+
+}
