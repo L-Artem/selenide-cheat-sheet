878d16e12a5d196a8ffebed6ebad50e2e7cd2cd6
L-Artem <plazalavz@gmail.com>
2024-02-10 12:11:13 +0300

    004 - 11less - OOP

--- src/test/java/QAGU/_11_Less_OOP/Main.java
+++ src/test/java/QAGU/_11_Less_OOP/Main.java
@@ -1,17 +1,27 @@
 package QAGU._11_Less_OOP;
 
 import QAGU._11_Less_OOP.data.Card;
-import QAGU._11_Less_OOP.data.VisaCard;
+import QAGU._11_Less_OOP.data.UnionPayCard;
 
 import static QAGU._11_Less_OOP.data.Country.RU;
 
+// 1:13:30 - ENUM
+//https://www.youtube.com/watch?v=UufkoVcMnX8#t=01h13m30s
+// Изучаем Java - Сьерра Кэти и Бэйтс Берт  ООП
+// Философия Java. 4-е полное изд. | Эккель Брюс
+// Java: эффективное программирование | Блох Джошуа
+
 public class Main {
     public static void main(String[] args) {
-        Card visacard = new VisaCard();
-        visacard.setBalance(100);
-        visacard.payInCountry(RU,75);
-        System.out.println(visacard.getBalance());
-        System.out.println();
+        invoke(new UnionPayCard());
 
+        System.out.println();
+        System.out.println(RU.getRusCountryName());
+    }
+
+    public static void invoke(Card card){
+        card.setBalance(100);
+        card.payInCountry(RU, 75);
+        System.out.println("Баланс: " + card.getBalance());
     }
 }
